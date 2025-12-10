40394f09563478cbcf8fe2e56aeb392694d684fd
João Victor <joaovictordsantosr@gmail.com>
2025-12-10 18:19:47 -0300

    feat: add Filter function

--- utils/utils.go
+++ utils/utils.go
@@ -129,3 +129,14 @@ func CreateMenu(options []string, menuTitle ...string) {
 		fmt.Printf("%d. %s\n", i+1, option)
 	}
 }
+
+// Função para filtragem de uma lista genérica de dados
+func Filter[T any](data []T, f func(T) bool) []T {
+	fltd := make([]T, 0)
+	for _, e := range data {
+		if f(e) {
+			fltd = append(fltd, e)
+		}
+	}
+	return fltd
+}
